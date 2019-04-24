9ba7b9bdeeb9c187c7b040cadb5917ec442c8dcb
Florian Thiévent <florianjeremias@gmail.com>
2019-04-24 12:11:47 +0200

    [fth] added roles and users to bootstrap

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -3,7 +3,22 @@ package webec
 class BootStrap {
 
     def init = { servletContext ->
+
+        SecRole adminRole = save(SecRole.findOrCreateWhere(authority: 'ROLE_ADMIN'))
+        SecRole studentRole = save(SecRole.findOrCreateWhere(authority: 'ROLE_STUDENT'))
+
+
+        SecUser admin = save(new SecUser(username: 'admin', password: 'admin'))
+        SecUser student = save(new SecUser(username: 'student@students.fhnw.ch', password: 'student'))
+
+        SecUserSecRole.create(admin, adminRole, true)
+        SecUserSecRole.create(student, studentRole, true)
     }
+
+    static save(domainObject) {
+        domainObject.save(failOnError: true, flush:true)
+    }
+
     def destroy = {
     }
 }

567fc2af534dc1069dbc640a3ed6092293eca05b
Florian Thiévent <florianjeremias@gmail.com>
2019-04-24 12:27:49 +0200

    [fth] added guest role

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -4,15 +4,18 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        SecRole adminRole = save(SecRole.findOrCreateWhere(authority: 'ROLE_ADMIN'))
-        SecRole studentRole = save(SecRole.findOrCreateWhere(authority: 'ROLE_STUDENT'))
+        SecRole adminRole = save(SecRole.findOrCreateWhere(authority: SecRole.ADMIN))
+        SecRole studentRole = save(SecRole.findOrCreateWhere(authority: SecRole.STUDENT))
+        SecRole guestRole = save(SecRole.findOrCreateWhere(authority: SecRole.GUEST))
 
 
         SecUser admin = save(new SecUser(username: 'admin', password: 'admin'))
         SecUser student = save(new SecUser(username: 'student@students.fhnw.ch', password: 'student'))
+        SecUser guest = save(new SecUser(username: 'guest', password: 'guest'))
 
         SecUserSecRole.create(admin, adminRole, true)
         SecUserSecRole.create(student, studentRole, true)
+        SecUserSecRole.create(guest, guestRole, true)
     }
 
     static save(domainObject) {

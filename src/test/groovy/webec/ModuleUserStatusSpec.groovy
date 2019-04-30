2aceb98b196fd0ae9c00d97f0eb4406d2697f72e
Florian Thiévent <florianjeremias@gmail.com>
2019-05-01 00:18:07 +0200

    [fth] added images

---
+++ src/test/groovy/webec/ModuleUserStatusSpec.groovy
@@ -0,0 +1,18 @@
+package webec
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ModuleUserStatusSpec extends Specification implements DomainUnitTest<ModuleUserStatus> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}

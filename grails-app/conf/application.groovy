c4c4f14be37b9e7818a05f9759182c7e323f697d
Florian Thiévent <florianjeremias@gmail.com>
2019-04-30 14:27:52 +0200

    [fth] added mime types and reformated

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -8,34 +8,41 @@ grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.securityConfigType = 'InterceptUrlMap'
 
 final statics = [
-	[pattern: '/error',          access: ['permitAll']],
-	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
-	[pattern: '/shutdown',       access: ['permitAll']],
-	[pattern: '/assets/**',      access: ['permitAll']],
-	[pattern: '/**/js/**',       access: ['permitAll']],
-	[pattern: '/**/css/**',      access: ['permitAll']],
-	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+        [pattern: '/error', access: ['permitAll']],
+        [pattern: '/index', access: ['permitAll']],
+        [pattern: '/index.gsp', access: ['permitAll']],
+        [pattern: '/shutdown', access: ['permitAll']],
+        [pattern: '/assets/**', access: ['permitAll']],
+        [pattern: '/**/js/**', access: ['permitAll']],
+        [pattern: '/**/css/**', access: ['permitAll']],
+        [pattern: '/**/images/**', access: ['permitAll']],
+        [pattern: '/**/favicon.ico', access: ['permitAll']]
 ]
 
 final chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
+        [pattern: '/assets/**', filters: 'none'],
+        [pattern: '/**/js/**', filters: 'none'],
+        [pattern: '/**/css/**', filters: 'none'],
+        [pattern: '/**/images/**', filters: 'none'],
+        [pattern: '/**/favicon.ico', filters: 'none'],
+        [pattern: '/**', filters: 'JOINED_FILTERS']
 ]
 
 final interceptUrlMap = statics + [
-		[pattern: "/login/auth",    access: ["permitAll"]],
-		[pattern: "/dbconsole/**", 	access: ['ROLE_ADMIN']], // allow Admin Only to access DBConsole
-		[pattern: "/map/**",    	access: ['ROLE_ADMIN', 'ROLE_STUDENT']],
-		[pattern: "/**",    		access: ['ROLE_ADMIN', 'ROLE_STUDENT']],
+        [pattern: "/login/auth", access: ["permitAll"]],
+        [pattern: "/user/register", access: ["permitAll"]],
+        [pattern: "/dbconsole/**", access: ['ROLE_ADMIN']], // allow Admin Only to access DBConsole
+        [pattern: "/map/**", access: ['ROLE_ADMIN', 'ROLE_STUDENT']],
+        [pattern: "/**", access: ['ROLE_ADMIN', 'ROLE_STUDENT']],
 ]
 
 
 grails.plugin.springsecurity.controllerAnnotations.staticRules = statics
 grails.plugin.springsecurity.filterChain.chainMap = chainMap
 grails.plugin.springsecurity.interceptUrlMap = interceptUrlMap
+
+
+grails.mime.types = [
+        json: ['application/json', 'text/json'],
+        xml : ['text/xml', 'application/xml']
+]
